8a6bc2e86a1c05fc3e56191786c1d77e07c2bf44
--replace-all <hrishikesh.shinde@konsultera.in>
2023-04-17 16:26:44 +0530

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# pdf-upload
+
+A Basic streamlit app to upload a pdf and display the response in the form of table.
---
+++ main.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+# Displays table(s) according to the response.
+def change_name_format(name):
+  return name.replace("_", " ").title()
+
+def get_column_name(keys):
+  return [change_name_format(key) for key in keys.keys()]
+
+def show_table(response):
+  response_item = response['response']['data']
+  for item in response_item:
+    if isinstance(response_item[item], dict):
+      df = pd.DataFrame(
+        [list(response_item[item].values())],
+        columns = get_column_name(response_item[item])
+      )
+    else: 
+      df = pd.DataFrame(
+        [list(i.values()) for i in response_item[item]],
+        columns = get_column_name(response_item[item][0])
+      )
+    st.title(change_name_format(item))
+    hide_table_row_index = """
+            <style>
+              thead tr th:first-child {display:none}
+              tbody th {display:none}
+            </style>
+            """
+    st.markdown(hide_table_row_index, unsafe_allow_html=True)
+    st.table(df)
+
+#Getting pdf input
+uploaded_pdf = st.file_uploader("Upload your pdf")
+if uploaded_pdf is not None:
+  url = "https://pivot-port-poldoc-health.attributum.com/api/ml_process"
+  files = {
+    "input_file" : uploaded_pdf
+  } 
+  data = {
+    "insurance_company" : "star health",
+    "data_type" : "Generic"
+  }
+  headers = {"Authorization":"Api-Key T33fvOdn.n2AO1NH9GmU2jL9066FEOQvIw9zSLJSc"}
+
+  #Post API call.
+  response = requests.post(url, files=files, data=data, headers=headers)
+
+  if response:
+    show_table(response.json())
+
